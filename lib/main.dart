ecaf66816e1246277f6791db1047a248b60c152f
ingjuan824 <jcarcamo@estrategiaswebcolombia.com>
2021-02-11 14:41:18 -0500

    plantilla base para iniciar un proyecto con get x pattern modificado por el ingeniero juan carcamo

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:get/route_manager.dart';
 import 'package:get_X_pattern/app/modules/splash/splash_binding.dart';
 import 'package:get_X_pattern/app/modules/splash/splash_page.dart';
 import 'package:get_X_pattern/app/routes/app_pages.dart';
+import 'package:get_X_pattern/app/utils/dependency_injection.dart';
 
 void main() {
+  // Realizamos nuestra iyeccion de dependencias 
+DependencyInjection.init();
   runApp(MyApp());
 }
 
